6dd78d3304406d9f87a9663d9108ea740ea779f0
Elena Fedoseeva <feya@ld.yandex.ru>
2017-01-23 02:59:53 +0300

    visual effects for sound buttons

--- src/components/noteBtn.js
+++ src/components/noteBtn.js
@@ -1,18 +1,26 @@
 import React, {
   Component
 } from 'react';
-
+import cx from 'classnames';
 import {connect} from 'react-redux';
 
 import {clickNote} from '../AC/clickNote';
 
 class clickNoteBtn extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {selected: false};
+  }
   render() {
     const {id}=this.props;
+    const cl=`sector${id}--selected`;
     return (
       <button
-        className={`sector sector${id}`}
+        className={cx(`sector sector${id}`,{[cl]:this.state.selected})}
         onClick={this.clickBtn}
+        onMouseDown ={this.mouseDownBtn}
+        onMouseUp ={this.mouseUpBtn}
       />
     );
   }
@@ -23,6 +31,18 @@ class clickNoteBtn extends Component {
     if (!lock) {
       clickNote(id);
     }
+  }
+  mouseDownBtn=(ev) => {
+    ev.preventDefault();
+    this.setState({
+      selected: true
+    });
+  }
+  mouseUpBtn=(ev) => {
+    ev.preventDefault();
+    this.setState({
+      selected: false
+    });
   }
 }
 
